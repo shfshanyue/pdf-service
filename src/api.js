c1183e8e5ed4abec7a0da8a861f020aba1f13483
wangxian <wangxian@innobuddy.com>
2017-12-19 11:23:24 +0800

    Add route: pdf

--- src/api.js
+++ src/api.js
@@ -1,17 +1,27 @@
 const router= require('koa-joi-router')
+const render = require('./pdf-util').render
 const api = router()
 const Joi = router.Joi
 
 api.prefix('/api')
 api.route({
-  path: '/',
+  path: '/pdf',
   method: 'GET',
   validate: {
     query: {
+      url: Joi.string().uri({ scheme: ['http', 'https'] }).required(),
+      cookies: Joi.array().items(Joi.object({
+        name: Joi.string().required(),
+        value: Joi.string().required(),
+        domain: Joi.string(),
+        expires: Joi.number(),
+        httpOnly: Joi.boolean()
+      }))
     }
   },
   async handler (ctx) {
-    ctx.body = ctx.request.query
+    ctx.set('Content-Type', 'application/pdf')
+    ctx.body = await render(ctx.query)
   }
 })
 
---
+++ src/pdf-util.js
@@ -0,0 +1,27 @@
+const puppeteer = require('puppeteer')
+const _ = require('lodash')
+
+async function render (options) {
+  const { url, cookies, pdf } = options
+  const browser = await puppeteer.launch({
+    executablePath: '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary',
+    // devtools: true
+  })
+  const page = await browser.newPage()
+  if (!_.isEmpty(cookies)) {
+    await page.setCookie(...cookies.map(cookie => ({
+      url,
+      ...cookie
+    })))
+  }
+  await page.goto(url, {
+    waitUntil: 'networkidle0'
+  })
+  const pdfFile = await page.pdf()
+  await browser.close()
+  return pdfFile
+}
+
+module.exports = {
+  render
+}
